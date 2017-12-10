3f3cca8747b4115f3ce406c2f973a467cc4bfe8a
jfrancodardengo <jfrancodardengo@gmail.com>
2017-12-10 18:20:06 -0200

    10-12-2017: Criação de fragments. Overview funcionando.

--- app/src/main/java/com/example/android/movies/TrailerAdapter.java
+++ app/src/main/java/com/example/android/movies/TrailerAdapter.java
@@ -32,6 +32,7 @@ public class TrailerAdapter extends RecyclerView.Adapter<VideoViewHolder>  {
 
     @Override
     public void onBindViewHolder(VideoViewHolder vh1, int position) {
+
         final Videos video = videos.get(position);
 
         if (position == 1) {
